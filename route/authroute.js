71cb02423d8bd6452be234d94f6a816269b6f091
timosdev99 <timothyisah4@gmail.com>
2024-07-04 14:20:02 +0100

    second commit

--- route/authroute.js
+++ route/authroute.js
@@ -3,6 +3,15 @@ const Router = express.Router()
 const jwt = require('jsonwebtoken')
 const user = require('../MODELS/users')
 const bcrypt = require('bcrypt')
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env,
+        pass: process.env
+    }
+});
 
 
 Router.post('/register', async(req, res) => {
@@ -68,6 +77,81 @@ Router.post('/login', async(req, res) => {
     }
 })
 
+Router.post('/sendotp', async (req, res) => {
+    const otp = Math.floor(100000 + Math.random() * 900000);
+    const { email } = req.body;
+    try {
+        const mailOptions = {
+            from: process.env,
+            to: email,
+            subject: 'OTP for verification',
+            text: `Your OTP for verification is ${otp}`
+        };
 
+        
+
+
+        transporter.sendMail(mailOptions,async function (error, info) {
+            if (error) {
+                console.log(error);
+                res.status(500).json({
+                    message: error.message
+                });
+            } else {
+                // save otp in db
+                const user = await  User.findOne({ email });
+                if(!user){
+                    return res.status(400).json({
+                        message: 'User not found'
+                    });
+                }
+                user.otp = otp;
+                user.save();
+
+                console.log('Email sent: ' + info.response);
+                res.json({
+                    message: 'Email sent successfully',
+                });
+            }
+        });
+    }
+    catch (err) {
+        res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
+Router.post('/changepassword', async (req, res) => {
+    const {email , otp , newpassword} = req.body;
+    try {
+          const user = await User.findOne({ email });
+
+            if(!user){  
+                return res.status(400).json({
+                    message: 'User not found'
+                });
+            }
+
+            if(user.otp !== otp){
+                return res.status(400).json({
+                    message: 'Invalid OTP'
+                });
+            }
+
+            user.password = newpassword;
+            user.otp = null;
+            await user.save();
+
+            res.json({
+                message: 'Password changed successfully'
+            });
+    }
+    catch (err) {
+        res.status(500).json({
+            message: err.message
+        });
+    }
+});
 
 module.exports = Router 
